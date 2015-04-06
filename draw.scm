e8638c89cd9054c8e37956934ed87aa2d76c7cd3
Jake A Barnwell <jb16@mit.edu>
2015-04-05 22:36:39 -0400

    Bugfixes in draw, adding slow-draw methods.

--- draw.scm
+++ draw.scm
@@ -1,12 +1,13 @@
 ;;;; Draw looks at the uniform representation and attempts to draw, using
 ;;;; Scheme's built-in graphics library, from an invariant representation.
 
+
 (define (draw:point? expr)
-  (and (tagged-list? expr 'draw:point)
+  (and (tagged-list? expr 'point)
        (= (length expr) 3)))
 
 (define (draw:line? expr)
-  (and (tagged-list? expr 'draw:line)
+  (and (tagged-list? expr 'line)
        (= (length expr) 5)))
 
 (define device #f)
@@ -14,6 +15,7 @@
   (set! device (make-graphics-device (car (enumerate-graphics-types)))))
 
 (define (draw:close-graphics!)
+  (graphics-close device)
   (set! device #f))
 
 
@@ -26,7 +28,7 @@
     (graphics-draw-point d x y)))
   
 (define (draw:line d expr)
-  (assert (draw:point? expr))
+  (assert (draw:line? expr))
   (assert d)
   (let ((x1 (draw:line:get 'x1 expr))
 	(y1 (draw:line:get 'y1 expr))
@@ -36,16 +38,29 @@
 
 (define (draw:point:get ele expr)
   (assert (draw:point? expr))
-  (cond ((eq? ele x1) (cadr expr))
-	((eq? ele x2) (caddr expr))
+  (cond ((eq? ele 'x) (cadr expr))
+	((eq? ele 'y) (caddr expr))
 	(else
 	 (error "Can't query ele from point" ele expr))))
 
 (define (draw:line:get ele expr)
   (assert (draw:line? expr))
-  (cond ((eq? ele x1) (cadr expr))
-	((eq? ele y1) (caddr expr))
-	((eq? ele x2) (cadddr expr))
-	((eq? ele y2) (caddddr expr))
+  (cond ((eq? ele 'x1) (cadr expr))
+	((eq? ele 'y1) (caddr expr))
+	((eq? ele 'x2) (cadddr expr))
+	((eq? ele 'y2) (fifth expr))
 	(else
 	 (error "Can't query ele from line" ele expr))))
+
+(define (draw:slow-line d expr speed)
+  (assert (draw:line? expr))
+  (assert d)
+  ;; todo... will fix soon.
+  (let ((x1 (draw:line:get 'x1 expr))
+	(y1 (draw:line:get 'y1 expr))
+	(x2 (draw:line:get 'x2 expr))
+	(y2 (draw:line:get 'y2 expr)))
+    (graphics-draw-line d x1 y1 x2 y2)))
+
+(define (draw:slow-point d expr speed)
+  (draw:point d expr))
