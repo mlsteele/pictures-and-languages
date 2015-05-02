1cc4114911fef395573da7224f19fef96dedf504
Miles Steele <miles@milessteele.com>
2015-05-02 15:33:56 -0400

    add some more hybrid stuff

--- hybrid.scm
+++ hybrid.scm
@@ -1,7 +1,7 @@
 ;;; A graphical description language which is a hybrid of others.
 ;;; This language includes our favorite parts from other languages:
 ;;; This includes:
-;;; - active transformation
+;;; - active transformation that applies to all commands
 ;;; - unwindable temporary transformations
 ;;; - automatic recursion base case detection for self-similar shapes
 ;;; - relative positioning and mirroring
@@ -20,6 +20,11 @@
     (point! 0 0))
   ... use transformation
   ... check if 1 is too small?
+  ... ur)
+
+(define (line! x1 y1 x2 y2)
+  ... use transformation
+  ... check if too small
   ... ur)
 
 (define (save-excursion thunk)
@@ -50,9 +55,10 @@
 (define (forward dist #!optional thunk)
   (translate 0 dist thunk))
 
-;;; Repeat a drawing task 'times using 'transformer in between each.
+;;; Repeat a drawing task 'times.
+;;; Each execution could change the transformation.
 ;;; Restore the transformation afterwards.
-(define (repeat times transformer thunk)
+(define (repeat times thunk)
   (save-excursion (lambda _
     (let loop ((n times)))
       (if (> n 0)
@@ -63,3 +69,11 @@
 
 (define (mirror-x thunk)
   ...)
+
+(define (square size)
+  (repeat 4
+    (lambda _
+      (line 0 0 0 size)
+      (forward 100)
+      (rotate 90))))
+
