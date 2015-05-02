e9d038264fa822c366f71dbde8204ea46bb030ad
Jake A Barnwell <jb16@mit.edu>
2015-05-02 17:04:44 -0400

    added t:flip

--- transform.scm
+++ transform.scm
@@ -64,3 +64,20 @@
     (%matrix:new `((,(cos theta) ,(sin (- 0 theta)) 0)
 		   (,(sin theta) ,(cos theta) 0)
 		   (0 0 1)))))
+
+(define (t:flip units val)
+  (let* ((theta (cond ((eq? units 'degrees)
+		       (degrees->rads val))
+		      ((eq? units 'radians)
+		       radians)
+		      (else
+		       (error "Units of incorrect type!" units))))
+	 (lx (cos theta))
+	 (ly (sin theta))
+	 (a (- (* lx lx) (* ly ly))) ;; householder transform
+	 (b (* 2 lx ly))
+	 (c (* 2 lx ly))
+	 (d (- (* ly ly) (* lx lx))))
+    (%matrix:new `((,a ,b 0)
+		   (,c ,d 0)
+		   (0 0 1)))))
