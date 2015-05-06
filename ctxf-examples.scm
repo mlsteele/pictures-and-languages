56994b80618e55df774d9c6767d1bce2ad10219d
Jake A Barnwell <jb16@mit.edu>
2015-05-05 23:00:39 -0400

    Add more ctxf examples, change draw so that it's a black bg with white shapes

--- ctxf-examples.scm
+++ ctxf-examples.scm
@@ -3,13 +3,13 @@
 (load "load")
 #|
  (ctxf '(
-	(startshape random)
+	(startshape r)
 	(shape random
-	       (rule ((square ())
-		      (random (dr -2 y 0.1 s 0.9 0.9))))
-	       (rule ((circle ())
-		      (random (dr 2  y 0.1 s 0.9 0.9)))))))
- 
+	       (rule ((square (dr (random 90)))
+		      (r (dr -2 y 0.1 s 0.9 0.9))))
+	       (rule ((circle (s 0.9 0.9))
+		      (r (dr 2  y 0.1 s 0.9 0.9)))))))
+
  (ctxf '(
 	 (startshape foo (dr 45))
 	 (shape foo (
@@ -83,3 +83,20 @@
 			       ))
 	  ))
 )
+
+(define (make-border)
+  (ctxf '(
+	  (startshape two-borders)
+	  (shape two-borders (
+			      (border-control (dr 180 y 4))
+			      (border-control)
+			      ))
+	  (shape border-control (
+				 (nearby-shapes (x (random 0.2)))
+				 (border-control (x (random 1.0) s (+ 0.8 (random 0.2)) (+ 0.8 (random 0.2))))
+				 ))
+	  (shape nearby-shapes (
+				(circle (y (+ -1.5 (random 1.5))))
+				))
+	  )))
+				 
